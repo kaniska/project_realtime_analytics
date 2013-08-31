81e9663ebe9977d3a13414c9c2d5f83e825773f4
kaniska <kaniska.mandal@gmail.com>
2013-08-31 01:14:08 -0700

    commit

---
+++ app.js
@@ -0,0 +1,301 @@
+var sys      = require("sys");
+var util     = require("util");
+// Use locally-installed modules
+require.paths.unshift('./node_modules');
+// Cloud Foundry
+var cf       = require("cloudfoundry");
+// MongoDB
+var mongoose = require("mongoose"),
+    Schema   = mongoose.Schema;
+// Redis
+var redis    = require("redis");
+// Express
+var express  = require("express");
+var app      = express.createServer();
+// Configure the app
+app.configure(function() {
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(app.router);	
+	app.use(express.static(__dirname + '/public'));
+	
+	app.set('view engine', 'jade');
+	app.set('running in cloud', cf.isRunningInCloud());
+
+	if(!cf.isRunningInCloud()) {
+		// Only use this in public for samples or development
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	}
+});
+// Socket.IO (sort of :)
+var io       = require("socket.io").listen(app, {
+	transports: ['xhr-polling'], 
+	transportOptions: {
+		'xhr-polling': {duration: 10000}
+	} 
+});
+
+// Mongoose Models --> 
+var safe_params = { j: 1, w: 1, wtimeout: 10000 };
+
+var StocksEventSchema = new Schema({
+	symbol: { type: String },
+	 price: { type: Number },
+	volume: { type: Number }
+},{safe:safe_params});
+mongoose.model('StocksEventSchema2', StocksEventSchema);
+
+
+var StocksSummarySchema = new Schema({
+	   symbol: { type: String },
+	timestamp: { type: Number },
+	      max: { type: Number },
+	      min: { type: Number },
+	  average: { type: Number },
+	   volume: { type: Number }
+},{safe:safe_params});
+mongoose.model('StocksSummarySchema2', StocksSummarySchema);
+
+var mongoConfig = cf.getServiceConfig("mongo-connector");
+var db = mongoose.createConnection("mongodb://" + mongoConfig.username + ":" + mongoConfig.password + "@" + mongoConfig.hostname + ":" + mongoConfig.port + "/" + mongoConfig.db);
+var mongooseTypes = require("mongoose-types")
+, useTimestamps = mongooseTypes.useTimestamps;
+StocksEventSchema.plugin(useTimestamps);
+
+// util.debug("mongo connection: "+"mongodb://" + mongoConfig.username + ":" + mongoConfig.password + "@" + mongoConfig.hostname + ":" + mongoConfig.port + "/" + mongoConfig.db);
+// Connect to Redis
+var redisConfig = cf.getServiceConfig("redis-connector");
+// util.debug("redis config: "+JSON.stringify(redisConfig));
+var redisClient = redis.createClient(redisConfig.port, redisConfig.hostname);
+var redisPublisher = redis.createClient(redisConfig.port, redisConfig.hostname);
+if(redisConfig.password) {
+	redisClient.auth(redisConfig.password);
+	redisPublisher.auth(redisConfig.password);
+}
+var watchers = {};
+redisClient.subscribe("redis-connector");
+redisClient.on("message", function(channel, json) {
+	
+	util.debug("<================ START ===============>");
+	
+	var data = JSON.parse(json);
+	var Summary = db.model('StocksSummarySchema2','stockssummaryinfo');
+	var TickerEvent = db.model('StocksEventSchema2', 'stocksdata');
+	var te = new TickerEvent({
+		symbol: data.symbol,
+		price: data.price,
+		volume: data.volume
+	});
+	TickerEvent.count({}, function( err, count){   
+    	// util.debug( "Records Count:", count ); 
+
+    if(count < STOCKS_INFO.length) {
+		te.save(function(err) {
+			if(err) {
+				throw(err);
+			}
+			util.debug("Step 1 : Got a Stock Symbol.."+JSON.stringify(data));
+			util.debug(te.createdAt); // Should be approximately now
+			var v_max = 0;
+			var v_min = 0;
+
+			
+			////////////// Step 1 - calculate Max
+			TickerEvent.find({ symbol : data.symbol }).sort('price', -1).limit(1).run( function(err, doc1) {			
+			if(doc1) {
+							util.debug(" Got Document1 "+doc1[0]+" for symbol "+data.symbol);
+							 v_max = doc1[0].price;
+							 util.debug(" Got Max Val : "+v_max);
+							 
+							 /////////// Step 3 - calculate Min
+							 TickerEvent.find({ symbol : data.symbol }).sort('price', 1).limit(1).run( function(err, doc2) {
+								 if(doc2) {
+										util.debug(" Got Document2 "+doc2[0]+" for symbol "+data.symbol);
+										 v_min = doc2[0].price;
+										 util.debug("Got Min Val : "+v_min);
+										 
+									//////// Step 4 - calculate Summary
+										 
+										 Summary.find({ symbol : data.symbol }).limit(1).run(function (err, doc3){
+											if(!err) {
+												util.debug("Found Summary " + doc3[0]);
+											 }
+											 else {
+												util.debug("Error: could not find Summary for " + data.symbol);
+											 }
+											if(!doc3 || doc3.length == 0) {
+												util.debug("Event : Create new Summary document ");
+												var summaryDoc = new Summary();
+												summaryDoc.symbol = data.symbol;		
+												  summaryDoc.timestamp = 1234;
+												  summaryDoc.max = v_max;
+												  summaryDoc.min = v_min;
+												  summaryDoc.volume = data.volume;
+												  summaryDoc.save(function(err) {
+														if(err) {
+															throw(err);
+														}
+												  });							 			
+			
+											}else {
+												  doc3[0].symbol = data.symbol;		
+												  doc3[0].timestamp = 1234;
+												  doc3[0].max = v_max;
+												  doc3[0].min = v_min;
+												  doc3[0].volume = doc3[0].volume + data.volume;
+												  doc3[0].save();
+												  
+												  util.debug("Step 4 : Saved the summary.."+JSON.stringify(doc3[0]));
+												  }
+											  util.debug("<================ END ===============>");
+											});
+										 
+								 }
+							});
+				
+				}
+				 
+			});	
+			
+		});
+	 }
+  });	 
+
+ io.broadcast(json);
+});
+
+var STOCKS_INFO = [
+      ["RIL",20,100],
+	  ["RIL",10,100],
+	  ["RIL",40,100],
+      ["TCS",10,100],
+      ["TCS",30,100],
+      ["INFY",10,100],
+      ["INFY",8,100],
+      ["INFY",28,100],
+      ["RIL",20,100],
+	  ["RIL",10,100],
+	  ["RIL",40,100],
+      ["TCS",10,100],
+      ["TCS",30,100],
+      ["INFY",10,100],
+      ["INFY",8,100],
+      ["INFY",28,100],
+	  ["RIL7",20,100],
+	  ["RIL7",10,100],
+	  ["RIL7",40,100],
+      ["TCS7",10,100],
+      ["TCS7",30,100],
+      ["INFY7",10,100],
+      ["INFY7",8,100],
+      ["INFY7",28,100],
+	  ["WIPRO1",30,100],
+	  ["WIPRO1",40,100],
+	  ["WIPRO1",60,100]
+	  ];
+
+function getSymbol(index) {
+	if(typeof STOCKS_INFO[index] == 'undefined') {
+		return undefined;
+	}
+	
+	return STOCKS_INFO[index][0];
+}
+
+function getPrice(index) {
+	if(typeof STOCKS_INFO[index] == 'undefined') {
+		return undefined;
+	}
+	return STOCKS_INFO[index][1];
+}
+
+function getVolume(index) {
+	if(typeof STOCKS_INFO[index] == 'undefined') {
+		return undefined;
+	}
+	return STOCKS_INFO[index][2];
+}
+
+var stocksEventSender;
+var index = 0;
+function sendStocksEvents() {
+	var symbolInfo = {
+		symbol: getSymbol(index), 
+		price: getPrice(index),
+		volume: getVolume(index)
+	};
+	//util.debug("sending ticker event: " + JSON.stringify(symbolInfo));
+	if(typeof symbolInfo.symbol != 'undefined') {
+		redisPublisher.publish("redis-connector", JSON.stringify(symbolInfo));
+	}
+	
+	var timeout = Math.round(Math.random() * 12000);
+	if(timeout < 3000) {
+		timeout += 10000;
+	}
+	index++;
+	if(index == STOCKS_INFO.length) {		
+	 index = 0;
+	}
+		//util.debug("Got an event");
+		stocksEventSender = setTimeout(sendStocksEvents, timeout);	
+	//}else {
+	//    stocksEventSender = null;
+	//}
+}
+
+// Routes
+app.get("/", function(req, resp) {
+	resp.render("home", {
+		pageTitle: "Indian Stocks Analysis"
+	});
+});
+
+app.get("/summary/:symbol", function(req, resp) {
+		/*mongoose.connection.on("open", function(){
+		util.debug("mongodb is connected!!");
+		});*/	
+	util.debug("<<======  SUMMARY  ======>>");	
+	util.debug("found symbol ..." + req.params.symbol);
+	
+	var StocksSummary2 = db.model('StocksSummarySchema2','stockssummaryinfo');
+	util.debug("Found TickerSummary");
+	var StocksEvent2 = db.model('StocksEventSchema2', 'stocksdata');
+	util.debug("Found TickerEvent");
+	StocksSummary2.findOne({ "symbol" : req.params.symbol}, function(err, summarydata) {		
+		if(err) {
+			throw(err);
+		}else{
+			util.debug("Good data >> "+summarydata);
+		}
+		if(!summarydata) {
+		    util.debug("Data undefined " + summarydata);
+		}else {		
+			util.debug("Found it!! "+summarydata);
+			var summaryDataJson = JSON.stringify(summarydata);
+			util.debug("Event 3 : Got the Summary "+ summaryDataJson);
+				StocksEvent2.find({ symbol : req.params.symbol }).limit(30).run(function (err, chartdata){
+					if(err) {
+						throw(err);
+					}
+					var chartDataJson = JSON.stringify(chartdata);
+					
+					util.debug("Complete data set ..."+ JSON.stringify({summary: summaryDataJson, chartdata: chartDataJson}));
+								
+			resp.send(JSON.stringify({summary: summarydata, chartData: chartdata}));
+			//resp.send(summaryDataJson);
+			});	
+        }			
+	});	
+});
+
+// Fetch Stocks Stream
+		io.on("connection", function(client) {
+			if(!stocksEventSender) {
+				sendStocksEvents();
+			}
+			util.debug("connection made..." + client);
+		});	
+
+// Listen for requests
+app.listen(cf.getAppPort());
---
+++ public/lib/app/app.js
@@ -0,0 +1,214 @@
+dojo.require("dojox.widget.DialogSimple");
+dojo.require("dijit.Dialog");
+dojo.require("dojo.data.ItemFileWriteStore");
+dojo.require("dojox.charting.Chart2D");
+dojo.require("dojox.charting.DataChart");
+dojo.require("dojox.charting.widget.Chart2D");
+dojo.require("dojox.charting.themes.PlotKit.blue");
+dojo.require("dojox.charting.plot2d.Columns");
+dojo.require("dojox.charting.axis2d.Default");
+dojo.require("dojox.charting.plot2d.Areas");
+dojo.require("dojox.charting.plot2d.Default");
+dojo.require("dojox.charting.DataSeries");
+dojo.require("dojox.charting.Chart");
+dojo.require("dojox.charting.themes.Tom");
+dojo.require("dojox.charting.plot2d.Lines");
+dojo.require("dojox.charting.plot2d.Markers");
+dojo.require("dojo.data.ItemFileWriteStore");
+dojo.require('dojox.charting.StoreSeries');
+dojo.require('dojox.charting.action2d.Tooltip');
+
+var eventDetails = "{}";
+var chart = null;
+var summaryContent = null;
+// http://www.sitepen.com/blog/2009/03/30/introducing-dojox-datachart/
+// http://www.sitepen.com/blog/2010/07/13/dive-into-dojo-charting/
+function createContent(stockdetails) {
+	//var data = dojo.toJson(stockdetails);
+	//console.log("6. Rendering the Summary Data : "+stockdetails.summary);   
+	 
+		 summaryContent =  "<b> <font color='brown'>Summary :</font></b>&nbsp;&nbsp;&nbsp;&nbsp; <b> Symbol : </b><font color='blue'>"+stockdetails.summary.symbol+"</font>&nbsp;&nbsp;&nbsp;&nbsp;<b> Max : <font color='blue'>"+stockdetails.summary.max+"</font>&nbsp;&nbsp;&nbsp;&nbsp;<b> Min : </b><font color='blue'>"+stockdetails.summary.min+
+						   "</font>&nbsp;&nbsp;&nbsp;&nbsp;<b> Total Volume : </b><font color='blue'>"+stockdetails.summary.volume+"</font>";						   
+		 
+        return summaryContent;
+}
+
+var yAxisLabelFunc = function(text, value, precision){
+   return "Rs. "+text;
+};
+
+//https://dojotoolkit.org/reference-guide/1.8/dojox/charting.html
+function showDojoChart() {
+	//dojo.byId("chartUI").innerHTML = "<div id='chartNode' style='width:800px;height:400px;'></div>";
+	//console.log("Created the Chart Node ...");
+	
+	var data = dojo.toJson(eventDetails);
+	console.log("Received event data ..."+data);
+	
+	var chartdetails = eventDetails.chartData;
+	//console.log("Going to iterate over the chartdata ...."+chartdetails);
+	console.log("Chart data length ..."+chartdetails.length);
+	//,\"idAttribute\":\"entryindex\"
+	var chartData = "{\"identifier\":\"entryindex\",\"label\":\"Price\",\"items\":[";
+	var chartData2 = []; 
+	//
+	for(var i=0; i<chartdetails.length; i++){
+		var entry  = chartdetails[i];
+		  console.log(entry, "at index", i);
+		  var date = new Date(entry.updatedAt);
+			var hours = date.getHours();
+			var minutes = date.getMinutes();
+			var seconds = date.getSeconds();
+			// will display time in 10:30:23 format
+			var formattedTime = hours + ':' + minutes + ':' + seconds;
+			 //X axis value			 
+			 console.log("Formatted Time : "+formattedTime);
+			 // Y -Axis Value
+			 console.log("Price  : "+entry.price);
+			 chartData = chartData + "{"+"\"entryindex\": "+ i +", \"price\": "+ entry.price +"}";
+			 chartData2[i] = entry.price;
+			 
+			 if(i <chartdetails.length -1) {
+				chartData =chartData + ", ";
+				//chartData2 = chartData2+ ",";
+			 }
+		}
+	chartData = chartData + "]}";
+	
+	//chartData2 = chartData2 + "]";
+	console.log("chart Data 1 ..."+chartData);
+	console.log("chart Data 2 ..."+chartData2);
+	
+	var mystore= new dojo.data.ItemFileWriteStore({data: chartData});
+	
+    // Define the data
+   // var chartData1= [10,20,30,40,50];
+	// var chartData3 = [{x: 0, y: 20}, {x: 1, y: 10}, {x: 2, y: 40}, {x: 3, y: 20}, {x: 4, y: 10}, {x: 5, y: 40}, {x: 6, y: 20}, {x: 7, y: 10}, {x: 8, y: 40}, {x: 9, y: 20}];
+     
+    // Create the chart within it's "holding" node
+    console.log("Creating Chart Element ...");
+    dojo.byId("chartNode").style.width = "60%";
+    //var chart = new Chart("chartNode");
+   
+    console.log("Initialized Chart Element...");
+    
+	if(!chart) {
+		chart = new dojox.charting.Chart("chartNode", {
+		  title: "Price Variation(Stocks/Mutual Fund)",
+		  titlePos: "top",
+		  titleGap: 25,
+		  titleFont: "normal normal normal 15pt Arial",
+		  titleFontColor: "orange"
+		});
+    
+		// Set the theme
+		//chart.setTheme(theme);
+		// Add the only/default plot 
+		chart.addPlot("default", {
+			type: "Areas",
+			markers: true
+		});
+		console.log("Added the plot ...");
+		// Add axes
+		chart.addAxis("x");	
+		//chart.addAxis("x", {min: 0, max: 10, majorLabels: true, minorTicks: false, minorLabels: false, 
+		//microTicks: false, natural: false, fixed: true,
+		//majorTick: {color: "red"},
+		//minorTick: {stroke: "black"}});  
+		chart.addAxis("y", { min: 5, max: 50, vertical: true, labelFunc : yAxisLabelFunc});
+		// Add the series of data
+		//chart.addSeries("y", new DataSeries(mystore, {query: {price: "*"}}, "price"),{stroke: 'red', fill: 'pink'});
+		chart.addSeries("Stock Price ", chartData2);	
+		//chart.setStore(mystore, {entryindex:"*"}, "price");	
+		console.log("Rendering the Chart...");
+	}else {
+	   chart.updateSeries("Stock Price ", chartData2);
+	}
+	//tooltips!
+	new dojox.charting.action2d.Tooltip(chart,"default");
+	// Render the chart!
+	chart.render();
+	console.log("Done with the Chart...");	
+}
+
+function handleSymbolClick(evt) {	
+	var symbol = evt.target.attributes["data"].value;
+	
+	var getTickersummary = (function(){
+	    var tickersummary;
+	    return function(){
+	        if(!tickersummary){
+	        	tickersummary = dojo.xhrGet({
+	                url: "/summary/" + symbol,
+	                handleAs: "json"
+	            });
+	        }
+	        return tickersummary;
+	    };
+	})();
+	 
+	dojo.when(getTickersummary(), function(tickersummary){
+	    // This callback will be run after the request completes
+	 
+		console.log("3. Summary Report : "+dojo.toJson(tickersummary)); 
+		dojo.byId("reportUI").innerHTML = createContent(tickersummary); 
+		// "<table> <tbody> <tr> <td> 'Hello World !' </td> </tr> </tbody> </table>";
+		console.log("Show the chart...");
+		
+		// Show Chart
+		//showDojoChart(tickersummary);
+		eventDetails = tickersummary;
+		dojo.ready(showDojoChart());
+		//showGoogleChart();	
+	});
+	
+}
+
+dojo.addOnLoad(function() {
+	var socket = new io.Socket();
+	socket.connect();
+	socket.on("message", function(msg) {
+	//	var jade = require('jade');
+		var data = dojo.fromJson(msg);
+		var html = dojo.byId("ticker-symbols").innerHTML;
+		dojo.byId("ticker-symbols").innerHTML = "<div class='ticker-data'>" +
+				"<div class='ticker-symbol'><a class='ticker-symb-link' href='#' data='" + data.symbol + "' title='Stock Symbol Analysis'>" + data.symbol + "</a></div>" +
+				"<div class='ticker-meta ticker-price'>Price: $" + data.price + "</div>" +
+				"<div class='ticker-meta ticker-volume'>Volume: " + data.volume + "</div>" +
+			"</div>" + html;
+
+		console.log("pre click-event stage ...");
+		
+		dojo.query(".ticker-symb-link").onclick(handleSymbolClick);
+
+		console.log("post click-event stage ...");
+		
+		var nodes = dojo.query("#ticker-symbols .ticker-data")
+		var total = 0;
+		var d = dojo;
+		var lastWidth = 0;
+
+		nodes.forEach(function(node, index, nodes) {
+			var style = d.getComputedStyle(node);
+			var width = parseInt(style.width.replace(/px/, ""));
+			total += width;
+			lastWidth = width;
+		});
+		total += lastWidth;
+
+		var newRight = (lastWidth * -1);
+		dojo.style("ticker-symbols", {
+			width: (total + (total/2)) + "px", 
+			right: newRight + "px"
+		});
+		dojo.anim("ticker-symbols", {
+			right: {
+				start: newRight,
+				end: 16,
+				unit: "px"
+			}
+		});
+		///////////////////////////
+	});
+	
+});
